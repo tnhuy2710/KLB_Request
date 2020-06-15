e8db203c0f08f41eb2c6e24b7a65055187b86302
tnhuy2710 <31790145+tnhuy2710@users.noreply.github.com>
2020-06-15 14:31:23 +0700

    Add files via upload

--- CoreApi/Models/Form.cs
+++ CoreApi/Models/Form.cs
@@ -6,7 +6,7 @@ namespace CoreApi.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-
+            
         public int SheetIndex { get; set; }
         public string FileType { get; set; }
 
